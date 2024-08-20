d04b3f587b86cb993fe22d485580041543402696
Abdul Kadir <ak8871639@gmail.com>
2024-08-20 10:30:06 +0530

    50 commit

--- modals/debitNodeModal.js
+++ modals/debitNodeModal.js
@@ -10,7 +10,7 @@ const debitnoteS=mongoose.Schema({
       required:[true,'company name is required']
       
      },
-    suplierDetail:{
+    clientDetail:{
        
         client:{
             type:String,
