faf59aa53fb26bb769458ffde572b21d7f48fd9b
abdul <ak8871639@gmail.com>
2024-10-30 11:50:50 +0530

    3005

--- routes/ledgerRoute.js
+++ routes/ledgerRoute.js
@@ -3,6 +3,8 @@ const Invoice = require('../modals/invoiceModal')
 const DebitNote = require('../modals/debitNodeModal')
 const CebitNote=require('../modals/creditNoteModal')
 const ClientPayment=require('../modals/clientPayment/clientPayment')
+const Inward=require('../modals/store/inwardModal')
+const SuppPayment=require('../modals/supplierPayment/supPayment')
 router = express.Router();
 router.get('/allTransaction/:companyname/:cid',async(req,res)=>{
   let finalarr=[];
@@ -31,10 +33,31 @@ router.get('/allTransaction/:companyname/:cid',async(req,res)=>{
         },[])
 
 
+       prd='';
+        let carr=invoiceData.reduce((acc,curr)=>{
+         if(curr.invoiceDetail.invoiceDate==prd){
+            let f=acc.find(elem=>elem.date==curr.invoiceDetail.invoiceDate)
+            f.invarr.push({mov:curr.mov,total:curr.total})
+            return acc
+
+        
+         }
+         else{
+            prd=curr.invoiceDetail.invoiceDate
+            let js={mov:curr.mov,total:curr.total}
+            acc.push({date:curr.invoiceDetail.invoiceDate,invarr:[js]})
+           
+            return acc
+
+         }
+       },[])
+   
+
+
 
 
     pr=''
-    let paymentData=await ClientPayment.find({companyname:companyname,cid:cid})
+   let paymentData=await ClientPayment.find({companyname:companyname,cid:cid})
    let arr= paymentData.reduce((acc,curr)=>{
          if(curr.paymentDate==prd){
             let f=acc.find(elem=>elem.date==curr.paymentDate)
@@ -53,27 +76,9 @@ router.get('/allTransaction/:companyname/:cid',async(req,res)=>{
 
          }
        },[])
-     /*  narr.forEach((elem, index) => {
-        let f=arr.find(elem2=>{
-            if(elem2.date==elem.date){
-            
-                return true
-            }
-            else{
-               let js={date:elem2.date,parr:elem2.parr}
-               // narr.push(js)
-               console.log(elem2.date)
-                return false
-
-            }
-        }
-        )
     
-      if(f){elem.parr=f.parr}
-       
-    }); */
      
-    res.send({invoice:narr,payment:arr})
+    res.send({invoice:narr,payment:arr,credit:carr})
     }
     catch(err){
      res.send(err.message)
@@ -83,6 +88,80 @@ router.get('/allTransaction/:companyname/:cid',async(req,res)=>{
    
 
 })
+router.get('/allTransactionSupp/:companyname/:sid',async(req,res)=>{
+   
+     try{
+     let {companyname,sid}=req.params
+     let prd=''
+     let inwardData=await Inward.find({companyname:companyname,'sid':sid})
+     let narr=inwardData.reduce((acc,curr)=>{
+           if(curr.inwardCreated==prd){
+              let f=acc.find(elem=>elem.date==curr.inwardCreated)
+              f.invarr.push({mov:curr.mov,total:curr.total})
+              return acc
+           }
+           else{
+              prd=curr.inwardCreated
+              let js={mov:curr.mov,total:curr.total}
+              acc.push({date:curr.inwardCreated,invarr:[js]})
+              return acc
+  
+           }
+         },[])
+ 
+     pr=''
+   let paymentData=await SuppPayment.find({companyname:companyname,sid:sid})
+    let arr= paymentData.reduce((acc,curr)=>{
+          if(curr.paymentDate==prd){
+             let f=acc.find(elem=>elem.date==curr.paymentDate)
+             let js={inwardList:curr.inwardList}
+             f.parr.push(js)
+             return acc
+          }
+          else{
+             prd=curr.paymentDate
+             let js={inwardList:curr.inwardList}
+           
+             acc.push({date:curr.paymentDate,parr:[js]})
+             return acc
+ 
+          }
+        },[])
+      /*  narr.forEach((elem, index) => {
+         let f=arr.find(elem2=>{
+             if(elem2.date==elem.date){
+             
+                 return true
+             }
+             else{
+                let js={date:elem2.date,parr:elem2.parr}
+                // narr.push(js)
+                console.log(elem2.date)
+                 return false
+ 
+             }
+         }
+         )
+     
+       if(f){elem.parr=f.parr}
+        
+     }); */
+      
+     res.send(narr)
+     }
+     catch(err){
+      res.send(err.message)
+     }
+ 
+     
+    
+ 
+ })
+ 
+
+
+
+
 
 module.exports=router
 
