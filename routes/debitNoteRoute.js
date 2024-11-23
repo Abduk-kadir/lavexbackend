2c8393ab3ec55b267b85ada88fad3f214934d40d
abdul <ak8871639@gmail.com>
2024-11-23 15:01:57 +0530

    3091

--- routes/debitNoteRoute.js
+++ routes/debitNoteRoute.js
@@ -59,7 +59,7 @@ router.post('/debitNoteCreate',async(req,res)=>{
         let itmnamearr=onAccount==false? req.body.item.map(elem=>elem.name).join():"0"
         let itmqtyarr=onAccount==false?req.body.item.map(elem=>elem.quantity).join():"0"
         let str=`Credit note is for client ${req.body.clientDetail.client} created and item is ${itmnamearr} and quantity is ${itmqtyarr} goes to client `
-        let j={companyname:type,itemId:max,actionType:'CREATE',changedBy:"ABDUL",changeDetails:str,model:"CreditNote"}
+        let j={companyname:type,itemId:max,actionType:'CREATE',changedBy:"ABDUL",changeDetails:str,model:"DebitNote"}
         console.log(j)
         let log=new Logs(j) 
         await log.save()
