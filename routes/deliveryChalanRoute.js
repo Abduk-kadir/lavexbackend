93bd192e5f148f8bc6b6e3b2550fd86504a6e5a1
abdul <ak8871639@gmail.com>
2024-11-25 14:57:23 +0530

    3099

--- routes/deliveryChalanRoute.js
+++ routes/deliveryChalanRoute.js
@@ -21,7 +21,15 @@ router.put('/deliveryUpdate/:id/:companyname',async(req,res)=>{
 
      }
      else{
-       await DeliveryChalan.findByIdAndUpdate(req.params.id,req.body, {runValidators: true })
+      let rs=await DeliveryChalan.findByIdAndUpdate(req.params.id,req.body, {runValidators: true })
+       let itmnamearr=rs.item.map(elem=>elem.name)
+       let itmqtyarr=rs.item.map(elem=>elem.quantity)
+       let str=`ivoice no ${rs.mov}  and that have item ${itmnamearr.join(',')} and quantity is ${itmqtyarr.join(',')} is deleted `
+       let j={companyname:rs.companyname,itemId:rs.mov,actionType:'DELETE',changedBy:"ABDUL",changeDetails:str,model:"Invoice"}
+       console.log(j)
+       let log=new Logs(j) 
+       await log.save()
+       await log.save()
        res.send({
         message:"data is successfully updated",
         success:true,
