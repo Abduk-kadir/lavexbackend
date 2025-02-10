3f4c447ab5b27677c88bc9a1bd6d9f5b1d62cc2c
abdul <ak8871639@gmail.com>
2025-02-10 11:16:53 +0530

    salesman commision

--- modals/drop/salesMan.js
+++ modals/drop/salesMan.js
@@ -3,6 +3,10 @@ let salesmanSchema=mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    com:{
+        type:Number,
+        required:[true,'commission is required']
     }
 })
 module.exports=mongoose.model('SalesMan',salesmanSchema)
